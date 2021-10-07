cffb6ba2ac12bb9528f61766fe5dd4b6cb33e27c
kevin20031120 <91889893+kevin20031120@users.noreply.github.com>
2021-10-07 13:57:55 +0200

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-x = "Hello"
-y = "Hi"
-
-r = requests.post('https://api.telegram.org/bot2017942575:AAFpsjlJlbzyG8DOZhdYZV6ugalmBke0RGU/sendMessage?chat_id=-517589081&text=%s'  % y)
